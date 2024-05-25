eaa6647a9cbbef1214b7f9960760009f1cb27008
JustRoxy <JustRoxyOsu@inbox.ru>
2024-05-25 17:27:34 +0500

    feat: ban FEM2

--- Unfair/Services/UnfairBannedTournament.cs
+++ Unfair/Services/UnfairBannedTournament.cs
@@ -5,7 +5,7 @@ namespace Unfair.Services;
 
 public class UnfairBannedTournament : IBannedTournament
 {
-    private static readonly HashSet<string> BannedAcronyms = ["ETX", "o!mm", "PSK", "TGC", "NDC2"];
+    private static readonly HashSet<string> BannedAcronyms = ["ETX", "o!mm", "PSK", "TGC", "NDC2", "FEM2"];
 
     public bool IsBanned(TournamentMatch match)
     {
