4fc2363f2f110ce08e9508eee1641fd7e669bff8
JustRoxy <JustRoxyOsu@inbox.ru>
2025-02-18 17:10:44 +0300

    feat: remove ROMAI tournament bot

--- Unfair/Services/UnfairBannedTournament.cs
+++ Unfair/Services/UnfairBannedTournament.cs
@@ -5,7 +5,7 @@ namespace Unfair.Services;
 
 public class UnfairBannedTournament : IBannedTournament
 {
-    private static readonly HashSet<string> BannedAcronyms = ["ETX", "o!mm", "PSK", "TGC", "NDC2", "FEM2"];
+    private static readonly HashSet<string> BannedAcronyms = ["ETX", "o!mm", "PSK", "TGC", "NDC2", "FEM2", "ROMAI"];
 
     public bool IsBanned(TournamentMatch match)
     {
